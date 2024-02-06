2b1ef07d34c031b32a0e5b479e72c7ef3e2273fb
IsaacQH <isaacqwert@gmail.com>
2024-02-05 20:05:35 -0600

    Servidor, Express y MongoDB

---
+++ app.js
@@ -0,0 +1,33 @@
+//ARCHIVO DE EXPRESS: Se hace la declaración de express y los protocolos necesarios
+
+'use strict'
+
+var express = require('express')        //Carga libreria express y se declara el objeto
+var bodyParse = require('body-parser')  //Carga libreria body parse (para hacer transofrmaciones) y se declara el objeto
+
+var app = express()
+
+//ARCHIVOS DE RUTAS
+
+//MIDDLEWARES: Son capas que se ejecutan antes de ejecutar el resultado de la peticion
+
+app.use(bodyParse.urlencoded({extended:false}))      //Configuracion NECESARIA para funcionalidad para bodyParse
+app.use(bodyParse.json())                            //Convierte todo lo que llega a un objeto JSON en express
+
+//CORS
+
+//RUTAS
+app.get('/', (req, res) =>{                    //función get de http 
+    res.status(200).send(                         //hace res (envia la respuesta) con un mensaje
+        "<h1>HOME</h1>"
+    )
+})
+
+app.get('/test', (req, res) =>{                    //función get de http 
+    res.status(200).send({                         //hace res (envia la respuesta) con un mensaje
+        message: "API node.JS en ruta Test"
+    })
+})
+
+//EXPORTAR
+module.exports = app
--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-//ARCHIVO PRINCIPAL DE EL PROYECTO
+//ARCHIVO PRINCIPAL DE EL PROYECTO: Se haran las funcionalidades principales y las conexiones requeridas.
+
+
+'use strict'
+
+var mongoose = require('mongoose')    //Carga modulo o libreria mongoose, se crea objeto de Mongoose
+var app = require('./app')            //importa en variable el valor de app.js
+var port = 3700                       //Puerto que queremos asignar para el proyecto
+//--------------------------------------------------------------------------------------------------------------
+mongoose.Promise = global.Promise     //Inidcamos que se realizará una promesa en mongoose
+mongoose.connect('mongodb://127.0.0.1:27017/Portafolio')  //Conecta a la db 127.0.0.1:27017 = localhost:27017
+        .then(() =>{                                      //Promesa que ejecuta si todo sale bien
+
+            console.log("Conexion a base de datos establecida con exito")
+
+            //CREACION DEL SERVIDOR ----------------------------------
+            app.listen(port,() =>{
+                console.log("Servidor corriendo correctamente en la url localhost:3700")
+            })
+
+
+
+        })
+        .catch((err) => {                                //Promesa que captura el error
+            console.log(err)
+        })
